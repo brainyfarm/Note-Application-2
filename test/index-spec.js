85cdc5e1bf34ac6251d4c052ac2ab70c31f48367
brainyfarm <brainyfarm@gmail.com>
2016-08-18 13:20:54 +0100

    Fixed test setup bugs

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,3 +1,13 @@
 var chai = require('chai');
 var expect = chai.expect;
-var notes = require('../');
+var assert = chai.assert;
+var noteApplication = require('../index');
+
+
+describe("Test cases for not application ", function(){
+	it("Test to be sure note", function(){
+
+		assert.equal(noteApplication("Olawale").author, "Olawale")
+	})
+})
+
